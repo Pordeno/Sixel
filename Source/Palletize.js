d87f9c844cb3233c683a1e0db4e514b27c8ccb00
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2022-11-20 19:22:47 -0500

    Combined unify & reducedetail

---
+++ Source/Palletize.js
@@ -0,0 +1,67 @@
+
+import hash from '../Source/Hash.js'
+
+const { round , min } = Math;
+
+
+export default function unify ( colors ){
+    
+    colors = [ ... colors ];
+    
+    let palette = [];
+    let pixels = [];
+    
+    let coarseness = 1;
+    
+    const coarser = ( channel ) =>
+        round(channel / coarseness) * coarseness
+        
+    const reduce = ([ r , g , b ]) => [
+        coarser(r) ,
+        coarser(g) ,
+        coarser(b)
+    ]
+    
+        
+    function retry (){
+        
+        const unique = [];
+        palette = [];
+        pixels = [];
+        
+        
+        for ( const color of colors ){
+            
+            const reduced = reduce(color);
+            
+            const id = hash(reduced);
+            
+            let index = unique.indexOf(id);
+
+            if(index < 0){
+                
+                if(unique.length >= 256){
+                    coarseness++;
+                    return false;
+                }
+                
+                index = unique.length;
+                palette.push(color);
+                unique.push(id);
+            }
+
+            pixels.push(index);
+        }
+        
+        return true
+    }
+    
+    while (true)
+        if(retry())
+            break;
+        
+    
+    console.log('Final Coarseness:',coarseness);
+    
+    return [ palette , pixels ]
+}
